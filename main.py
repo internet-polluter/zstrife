86c67f11e2e8c5526fbc79ff2708ca0b76c748d9
internet-polluter <internet0polluter@gmail.com>
2025-12-04 09:59:03 -0700

    zendriver hello world

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
+import asyncio
+import zendriver as zd
+import time
+
+
+async def main():
     print("Hello WORLD!@!")
+    browser = await zd.start()
+    page = await browser.get('https://discord.com')
+    time.sleep(10)
+    await browser.stop()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
